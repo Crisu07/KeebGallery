9ba07685c26bbcf1e9d90e2b3249c3263dffeabd
ThatOneKidChris <chrisnguyen127@gmail.com>
2023-10-22 15:19:46 -0700

    Moved Images Import to new file
    Make things cleaner

---
+++ src/keebsImages.js
@@ -0,0 +1,34 @@
+import yeti5 from './keebs/yeti5.jpg'
+import yeti6 from './keebs/yeti6.jpg'
+import yeti4 from './keebs/yeti4.jpg'
+
+import eighty1 from './keebs/eighty1.jpg'
+import eighty2 from './keebs/eighty2.jpg'
+import eighty3 from './keebs/eighty3.jpg'
+
+import abasic1 from './keebs/abasic1.jpg'
+import abasic2 from './keebs/abasic2.jpg'
+import abasic3 from './keebs/abasic3.jpg'
+
+import tiger1 from './keebs/tiger1.jpg'
+import tiger2 from './keebs/tiger2.jpg'
+import tiger3 from './keebs/tiger3.jpg'
+
+import zoomr1 from './keebs/zoomr1.jpg'
+import zoomr2 from './keebs/zoomr2.jpg'
+import zoomr3 from './keebs/zoomr3.jpg'
+
+import zoom1 from './keebs/zoom1.jpg'
+import zoom2 from './keebs/zoom2.jpg'
+import zoom3 from './keebs/zoom3.jpg'
+
+const images = [
+    yeti5, yeti4, yeti6,
+    eighty1, eighty2, eighty3,
+    abasic3, abasic1, abasic2,
+    tiger3, tiger2, tiger1,
+    zoomr3, zoomr1, zoomr2,
+    zoom1, zoom3, zoom2,
+  ];
+
+export default images;
