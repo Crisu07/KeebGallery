090fcbf242c039dcfa34e7f8a1a2c8db8c0b702d
ThatOneKidChris <chrisnguyen127@gmail.com>
2024-01-18 23:54:12 -0800

    Commission Form
    form css done and works, need to link emailing to it

--- src/components/pages/Commission.jsx
+++ src/components/pages/Commission.jsx
@@ -3,35 +3,130 @@ import React, { useState } from 'react'
 import ReqForm from './ReqForm'
 
 export const Commission = () => {
-  const [firstname, setFirst] = useState('')
-  const [lastname, setLast] = useState('')
+  // Do all the inputs within one usestate rather than having a diff per input
+  // set up as object
+  const [values, setValues] = useState({
+    firstname:'',
+    lastname:'',
+    email:'',
+    discord:'',
+    subject:'',
+    keyboard:'',
+    switches:'',
+    info:'',
+    info2:'',
+  })
 
-  // console.log(firstname)
+  const handleSubmit = (e)=> {
+    e.preventDefault(); // prevents page refresh on hitting button
+
+    // const data = new FormData(e.target)
+    // console.log(Object.fromEntries(data.entries()))
+  }
+
+  // Commission form inputs
+  const inputs = [
+    {
+      id:1,
+      name:'First Name',
+      type:'text',
+      placeholder:'First Name',
+      label:'First Name'
+    },
+    {
+      id:2,
+      name:'Last Name',
+      type:'text',
+      placeholder:'Last Name',
+      label:'Last Name'
+    },
+    {
+      id:3,
+      name:'Email',
+      type:'text',
+      placeholder:'Email',
+      label:'Email'
+    },
+    {
+      id:4,
+      name:'Discord',
+      type:'text',
+      placeholder:'Discord',
+      label:'Discord'
+    },
+    {
+      id:5,
+      name:'Subject',
+      type:'text',
+      placeholder:'Subject',
+      label:'Subject'
+    },
+    {
+      id:6,
+      name:'Keyboard',
+      type:'text',
+      placeholder:'Keyboard',
+      label:'Keyboard'
+    },
+    {
+      id:7,
+      name:'Switches',
+      type:'text',
+      placeholder:'Switches',
+      label:'Switches'
+    },
+    {
+      id:8,
+      name:'Additional Info',
+      type:'text',
+      placeholder:'Please let me know what services you will need for the build along with other preferences such as spacebar layout, keyboard layout (ISO / Ansi), etc.',
+      label:'Additional Info'
+    },
+    {
+      id:9,
+      name:'Comments',
+      type:'text',
+      placeholder:'Please let me know any additional comments regarding your build.',
+      label:'Comments'
+    },
+  ]
+
+  const onChange = (e)=> {
+    setValues({ ...values, [e.target.name]: e.target.value})
+  }
+  // console.log(values)
 
   return (
     <>
       <div className='commissioninfocontainer'>
-      {/* <h1 className='commissiontitle'>Commission Request</h1> */}
+        {/* <h1 className='commissiontitle'>Commission Request</h1> */}
 
-      {/* Commissions Closed Portion - uncomment when open */}
-      <h1 className='commissiontitle'>Commission Request (Closed)</h1>
-      <p><strong>Commission requests will be ignored until further notice.</strong> There is a limit to how much commissions I can take on. Follow the <a href="https://discord.gg/t4q99vGpjh" target='_blank'>Discord</a> for further updates.</p>
+        {/* Commissions Closed Portion - uncomment when open */}
+        <h1 className='commissiontitle'>Commission Request (Closed)</h1>
+        <p><strong>Commission requests will be ignored until further notice.</strong> There is a limit to how much commissions I can take on. Follow the <a href="https://discord.gg/t4q99vGpjh" target='_blank'>Discord</a> for further updates.</p>
 
-      <p>I am able to build keyboards if all the parts are provided and delivered to me by the client. Further discussion on commission specifics can be discussed on <a href="https://discord.gg/t4q99vGpjh" target='_blank'>Discord</a> instead of filling this form.</p>
-      <strong>Please only fill out this form if you have already purchased or have all the parts on hand. I will not wait for any group buys or missing parts.</strong>
+        <p>I am able to build keyboards if all the parts are provided and delivered to me by the client. Further discussion on commission specifics can be discussed on Discord instead of filling this form.</p>
+        <strong>Please only fill out this form if you have already purchased or have all the parts on hand. I will not wait for any group buys or missing parts. </strong> 
+        <br />
+        <strong>If you have questions or want a quote on a certain build, please <a href="/contact">contact me</a> instead. This form is for commission requests specifically. Any troll or spam requests will be blocked from further commissions.</strong>
+        <br />
       </div>
-      <div className='commissionformcontainer'>
-        <form>
-          <ReqForm placeholder='First Name' setFirst={setFirst}/>
-          <ReqForm placeholder='Last Name'/>
-          <ReqForm placeholder='Email Address'/>
-          <ReqForm placeholder='Discord'/>
-          <ReqForm placeholder='Subject'/>
-          <ReqForm placeholder='Keyboard'/>
-          <ReqForm placeholder='Switches'/>
-          <ReqForm placeholder='Additional Keyboard Info'/>
-          <ReqForm placeholder='Comments'/>
-        </form>
+      <div>
+        <form className='commissionform' onSubmit={handleSubmit}>
+            {/* Map out the inputs and display each one rather than doing line by line */}
+            {/* ...input throws in all the values such as placeholder label etc */}
+            {/* value searches for the input name */}
+            {inputs.map((input) => (
+              <ReqForm 
+              key={input.id} 
+              {...input} 
+              value={values[input.name]} 
+              onChange={onChange} 
+              />
+            ))}
+            <br />
+            <button className='btn-commission btn-primary-commission'>Send</button>
+          </form>
       </div>
     </>
   )
--- src/components/pages/ReqForm.jsx
+++ src/components/pages/ReqForm.jsx
@@ -4,10 +4,19 @@ import './Commission.css'
 
 // makes it easier than doing a separate input per criteria
 const ReqForm = (props) => {
+  const {label, onChange, id, ...inputProps} = props;
+
+  // last two inputs should have an expandable textbox
+  const isText = id > 7;
   return (
-    <div>
-        <label>First Name</label>
-        <input placeholder={props.placeholder} onChange={e=>props.setFirst(e.target.value)} className='commissioninput'/>
+    <div className='reqForm'>
+      <label className='commissionlabel'>{label}</label>
+      {isText ? (
+        <textarea {...inputProps} onChange={onChange} className='commissiontextarea'/>
+      ):
+      (
+        <input {...inputProps} onChange={onChange} className='commissioninput'/>
+      )}
     </div>
   )
 }
